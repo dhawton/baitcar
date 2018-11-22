7f6d0a9f2cb73d3184c9ce093056e3f241561b04
Daniel Hawton <daniel@hawton.com>
2018-11-22 01:30:31 -0900

    Add Readme, License, resource LUA and change some build options

---
+++ Server/misc/__resource.lua
@@ -0,0 +1,21 @@
+﻿resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Baitcar'
+
+version '0.99.0'
+
+server_scripts {
+	'config.json',
+	'server/Baitcar.Server.net.dll',
+	'server/FetchJob.lua'
+}
+
+client_scripts {
+	'config.json',
+	'client/Baitcar.Client.net.dll'
+}
+
+dependencies {
+    'es_extended',
+    'esx_policeJob'
+}
