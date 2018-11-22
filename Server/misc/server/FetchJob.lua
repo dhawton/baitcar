40e3d51bd0869be94214a8544b87666949d5023c
Daniel Hawton <daniel@hawton.com>
2018-11-22 01:23:30 -0900

    Add misc directory for other files, aids in release builds

---
+++ Server/misc/server/FetchJob.lua
@@ -0,0 +1,12 @@
+﻿ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+RegisterServerEvent('Baitcar.getJob')
+AddEventHandler('Baitcar.getJob', function(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if xPlayer ~= nil and xPlayer.job ~= nil then
+		TriggerClientEvent('Baitcar.hasJob', xPlayer.job.name)
+	end
+end)
